b57c49ed0df7552d169ca3e3134d08a38a2630de
Roman Mushet <roman22mushet@gmail.com>
2022-02-18 22:11:21 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        TASK#1
+//     int x = 1000;
+//     int k = 1018;
+//     while (x != k) {
+//         System.out.println(x);
+//         x +=3;
+
+////        Task#2
+//        int x = 10;
+//        int k = -14;
+//        while (x != k) {
+//         System.out.println(x);
+//         x -=4;
+//     }
+//        Task#4
+//        int a=0;
+//        int b=0;
+//        Scanner scn=new Scanner(System.in);
+//        if(scn.hasNextInt()){
+//            a=scn.nextInt();
+//            while(a!=0){
+//                b=b+a%10;
+//                a/=10;
+//            }
+//
+//            System.out.println("Сумма всех чисел введенного числа равна " + b);
+//        }
+//        else System.out.println("Вы ввели не число");
+
+        //Array
+        //Task#1
+//        int a=0;
+//        for(int i=2;i<=20;i++){
+//            if(i%2==0)a++;
+//        }
+//
+//        int[] arr = new int[a];
+//        for(int i=2,b=0;i<=20;i++){
+//            if(i%2==0){
+//                arr[b]=i;
+//                System.out.print(arr[b]+" ");
+//                b++;
+//            }
+//        }
+//
+//        System.out.println(" ");
+//        for(int i=0;i<arr.length;i++){
+//            System.out.println(arr[i]);
+//        }
+        //Task#2
+//        int a=1;
+//        for(int i=1;i<=99;i++){
+//            if(i%2!=0)a++;
+//        }
+//
+//        int[] arr1 = new int[a];
+//        for(int i=1,b=0;i<=99;i++){
+//            if(i%2!=0){
+//                arr1[b]=i;
+//                System.out.print(arr1[b]+" ");
+//                b++;
+//            }
+//        }
+//
+//        System.out.println(" ");
+//        for(int i=arr1.length-1;i>=0;i--){
+//            System.out.print(arr1[i]+" ");
+//        }
+
+
+
+
+        }
+    }
+
+
